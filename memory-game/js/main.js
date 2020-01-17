cd44d3e4317aa43cb0ec0371d9b3109fc8ff9a9a
Finlay Whittington <FinlayWhittington@fins-imac.mynet>
2020-01-17 12:48:59 +0000

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -32,7 +32,6 @@ console.log("User flipped king")
 console.log("images/king-of-hearts.png")
 console.log("hearts")
 
-
 let checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
   		console.log("You found a match!");
@@ -40,15 +39,26 @@ let checkForMatch = function() {
   		console.log("Sorry, try again.");
 	}
 }
-let flipCard = function(cardId) {
 
-	flipCard(0);
-	flipCard(2);
-
+let flipCard = function() {
+	this.getAttribute('data-id')
 	console.log("User flipped " + cards[cardId].rank)
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage)
 	if (cardsInPlay.length === 2) {
 	checkForMatch();
 	};
 }
+
+let createBoard = function() {
+	for (let i = 0; i < cards.length; i++) {
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+createBoard();
 
